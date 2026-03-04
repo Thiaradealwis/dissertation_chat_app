31f6950390404ab8e15d829d4530434bcb9bfc00
Thiara de Alwis <mq23793@bristol.ac.uk>
2026-03-04 13:43:41 +0000

    lower threshold

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ const io = new Server(server, {
 
 const apiKey = await getOpenAIKey();
 const client = new OpenAI({ apiKey });
-const AI_RESPONSE_THRESHOLD = 10;
+const AI_RESPONSE_THRESHOLD = 6;
 
 const sessions = {};
 const colours = [
